7357916ba24b98c4c2d1231bdb4e24ea167dbede
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-02-14 21:31:34 +0000

    Mapping a user journey

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,4 +9,17 @@ class CompaniesController < ApplicationController
     @company = Company.find_by({ "id" => params["id"] })
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    @company.save
+    redirect_to "/companies"
+  end
+
 end
