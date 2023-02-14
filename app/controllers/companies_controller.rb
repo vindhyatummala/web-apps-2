b67bde8886cad128c228f24a0c894304ad0ceab2
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-02-14 20:07:01 +0000

    Moving prop code into controllers

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,8 +1,9 @@
 class CompaniesController < ApplicationController
 
   # TODO:
-  # def index
-  # end
+  def index
+    @companies = Company.all
+  end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,3 +1,10 @@
 class DiceController < ApplicationController
 
+    def index
+        @die1 = rand(1..6)
+        @die2 = rand(1..6)
+        @total = @die1 + @die2
+    #   render the file in /views/dice/index.html.erb
+    end
+
 end
