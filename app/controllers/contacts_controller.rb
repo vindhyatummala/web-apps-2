ab1b06b7088e6ef0aeab43dfc6583733c92e40bf
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-02-14 22:32:59 +0000

    Completing labs

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,19 @@ class ContactsController < ApplicationController
 
   def new
     @contact = Contact.new
+    @company = Company.find_by({ "id" => params["company_id"] })
+    @contact["company_id"] = @company["id"]
+  end
+
+  def create
+    @contact = Contact.new
+    @contact["first_name"] = params["contact"]["first_name"]
+    @contact["last_name"] = params["contact"]["last_name"]
+    @contact["email"] = params["contact"]["email"]
+    @contact["phone_number"] = params["contact"]["phone_number"]
+    @contact["company_id"] = params["contact"]["company_id"]
+    @contact.save
+    redirect_to "/companies/#{@contact["company_id"]}"
   end
 
 end
